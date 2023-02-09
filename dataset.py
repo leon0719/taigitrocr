f8bd6561595d65391d4822cc4f90634fecd4ccc3
leon0719 <leon0970865551@gmail.com>
2023-02-10 01:42:55 +0800

    update

--- dataset.py
+++ dataset.py
@@ -3,75 +3,33 @@ from PIL import Image
 from torch.utils.data import Dataset
 import torch
 
-class trocrDataset(Dataset):
-    def __init__(self, root_dir,df ,processor, max_target_length=256):
-        self.root_dir = root_dir
+class OCRDataset(Dataset):
+    def __init__(self, dataset_dir, df, processor, tokenizer, max_target_length=78):
+        self.dataset_dir = dataset_dir
         self.df = df
         self.processor = processor
         self.max_target_length = max_target_length
+        self.tokenizer = tokenizer
+
     def __len__(self):
         return len(self.df)
 
     def __getitem__(self, idx):
-            # 圖片名
-            file_name = self.df[0][idx]
-            #對應標籤
-            text = self.df[1][idx]
-            if file_name.endswith('jp'):
+        # get file name + text
+        file_name = self.df[0][idx]
+        text = self.df[1][idx]
+        if file_name.endswith('jp'):
                 file_name = file_name + 'g'
+        # prepare image (i.e. resize + normalize)
+        image = Image.open(os.path.join(self.dataset_dir,file_name)).convert("RGB")
+        pixel_values = self.processor(image, return_tensors="pt").pixel_values
+        # add labels (input_ids) by encoding the text
+        labels = self.tokenizer(text,
+                                padding="max_length",
+                                max_length=self.max_target_length).input_ids
 
-            image = Image.open(os.path.join(self.root_dir,file_name)).convert("RGB")
-            pixel_values = self.processor(image, return_tensors="pt").pixel_values
+        # important: make sure that PAD tokens are ignored by the loss function
+        labels = [label if label != self.tokenizer.pad_token_id else -100 for label in labels]
 
-            labels = self.processor.tokenizer(text,
-                                              padding="max_length",
-                                              truncation=True,
-                                              max_length=self.max_target_length).input_ids
-
-            labels = [label if label != self.processor.tokenizer.pad_token_id else -100 for label in labels]
-
-            encoding = {"pixel_values": pixel_values.squeeze(), "labels": torch.tensor(labels)}
-            return encoding
-
-
-def encode_text(text, max_target_length=128, vocab=None):
-    """
-    # list: ['<td>',"3","3",'</td>',....]
-    {'input_ids': [0, 1092, 2, 1, 1],
-    'attention_mask': [1, 1, 1, 0, 0]}
-    """
-    if type(text) is not list:
-       text = list(text)
-
-    text = text[:max_target_length - 2]
-    tokens = [vocab.get('<s>')] #-> [0]
-    unk = vocab.get('<unk>') # -> 3
-    pad = vocab.get('<pad>') # -> 1
-    mask = []
-    for tk in text:
-        token = vocab.get(tk, unk)  #-> 沒看過的分詞會被定義unk
-        tokens.append(token)
-        mask.append(1)
-
-    tokens.append(vocab.get('</s>')) #-> 2
-    mask.append(1)
-
-    if len(tokens) < max_target_length:
-        for i in range(max_target_length - len(tokens)):
-            tokens.append(pad)
-            mask.append(0)
-
-    return tokens
-    #return {"input_ids": tokens, 'attention_mask': mask}
-
-def decode_text(tokens, vocab, vocab_inp):
-    ##decode trocr
-    s_start = vocab.get('<s>') #-> [0]
-    s_end = vocab.get('</s>') # -> 2
-    unk = vocab.get('<unk>') # -> 3
-    pad = vocab.get('<pad>') # -> 1
-    text = ''
-    for tk in tokens:
-        if tk not in [s_end, s_start , pad, unk]:
-           text += vocab_inp[tk]
-    return text
+        encoding = {"pixel_values": pixel_values.squeeze(), "labels": torch.tensor(labels)}
+        return encoding
--- train.py
+++ train.py
@@ -1,16 +1,12 @@
 import os
 import argparse
 import pandas as pd
-from dataset import trocrDataset
+from dataset import OCRDataset
 from datasets import load_metric
 from transformers import (
     TrOCRProcessor,
-    ViTFeatureExtractor,
     VisionEncoderDecoderModel,
-    TrOCRForCausalLM,ViTConfig,
-    TrOCRConfig,AutoTokenizer,
-    TrOCRConfig,
-    ViTModel,
+    AutoTokenizer,
     default_data_collator,
     Seq2SeqTrainer,
     Seq2SeqTrainingArguments
@@ -23,9 +19,7 @@ def compute_metrics(pred):
     pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
     labels_ids[labels_ids == -100] = processor.tokenizer.pad_token_id
     label_str = tokenizer.batch_decode(labels_ids, skip_special_tokens=True)
-
     cer = cer_metric.compute(predictions=pred_str, references=label_str)
-
     return {"cer": cer}
 
 if __name__ == '__main__':
@@ -36,10 +30,10 @@ if __name__ == '__main__':
     parser.add_argument('--valid_label_text_path', default='/nfs/TS-1635AX/WorkSpace/leon/GPU5/train_data/valid_label.txt', type=str, help="驗證標籤位置")
     parser.add_argument('--per_device_train_batch_size', default=16, type=int, help="train batch size")
     parser.add_argument('--per_device_eval_batch_size', default=8, type=int, help="eval batch size")
-    parser.add_argument('--max_target_length', default=256, type=int, help="訓練文字字符數")
+    parser.add_argument('--max_target_length', default=78, type=int, help="訓練文字字符數")
     parser.add_argument('--num_train_epochs', default=25, type=int, help="訓練epoch數")
-    parser.add_argument('--eval_steps', default=20000, type=int, help="模型評估間隔數")
-    parser.add_argument('--save_steps', default=20000, type=int, help="模型保存間隔步數")
+    parser.add_argument('--eval_steps', default=40000, type=int, help="模型評估間隔數")
+    parser.add_argument('--save_steps', default=40000, type=int, help="模型保存間隔步數")
 
     args = parser.parse_args()
     print("train param")
@@ -50,36 +44,41 @@ if __name__ == '__main__':
     test_df = pd.read_csv(args.valid_label_text_path,sep='\t',header=None)
     print("train num:", len(train_df), "test num:", len(test_df))
 
-    # 調 encoder decoder 參數
+    trOCR_pre_train_model = "microsoft/trocr-base-handwritten"
+    nlp_hf_model = "microsoft/trocr-base-handwritten"
 
-    encoder_config = ViTConfig(image_size=384)
-    decoder_config = TrOCRConfig()
+    model = VisionEncoderDecoderModel.from_pretrained(trOCR_pre_train_model)
 
-    # load HuggingFace model
-    encoder = ViTModel(encoder_config, add_pooling_layer=False)
-    decoder = TrOCRForCausalLM(decoder_config)
-    model = VisionEncoderDecoderModel(encoder=encoder, decoder=decoder)
+    processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
 
-    # 圖像預處理
-    feature_extractor = ViTFeatureExtractor(size=encoder_config.image_size)
-    # 下載bert pre-trained model
-    tokenizer = AutoTokenizer.from_pretrained('bert-base-chinese')
-    processor = TrOCRProcessor(feature_extractor, tokenizer)
-    train_dataset = trocrDataset(root_dir=args.dataset_path,df=train_df ,processor=processor, max_target_length=args.max_target_length)
-    eval_dataset = trocrDataset(root_dir=args.dataset_path,df=test_df ,processor=processor, max_target_length=args.max_target_length)
+    tokenizer = AutoTokenizer.from_pretrained(nlp_hf_model)
 
-    #model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-stage1")
-    model.config.decoder_start_token_id = processor.tokenizer.cls_token_id
-    model.config.pad_token_id = processor.tokenizer.pad_token_id
-    model.config.vocab_size = processor.tokenizer.vocab_size
-    model.config.eos_token_id = processor.tokenizer.sep_token_id
-    model.config.max_length = 256
+    train_dataset = OCRDataset(
+    dataset_dir=args.dataset_path,
+    df=train_df,
+    tokenizer=tokenizer,
+    processor=processor,
+    max_target_length=args.max_target_length
+    )
+
+    eval_dataset = OCRDataset(
+    dataset_dir=args.dataset_path,
+    df=test_df,
+    tokenizer=tokenizer,
+    processor=processor,
+    max_target_length=args.max_target_length
+    )
+
+    model.config.decoder_start_token_id = tokenizer.cls_token_id
+    model.config.pad_token_id = tokenizer.pad_token_id
+    model.config.vocab_size = model.config.decoder.vocab_size
+
+    model.config.eos_token_id = tokenizer.sep_token_id
+    model.config.max_length = args.max_target_length
     model.config.early_stopping = True
     model.config.no_repeat_ngram_size = 3
     model.config.length_penalty = 2.0
     model.config.num_beams = 4
-
-
 
     cer_metric = load_metric("cer")
 
@@ -89,12 +88,12 @@ if __name__ == '__main__':
         per_device_train_batch_size=args.per_device_train_batch_size,
         per_device_eval_batch_size=args.per_device_eval_batch_size,
         fp16=True,
+        learning_rate=4e-5,
         output_dir=args.checkpoint_path,
         logging_steps=25,
         num_train_epochs=args.num_train_epochs,
         save_steps=args.save_steps,
-        eval_steps=args.eval_steps,
-        save_total_limit=5
+        eval_steps=args.eval_steps
     )
     # seq2seq trainer
     trainer = Seq2SeqTrainer(
